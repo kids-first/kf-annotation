eec9d2245b961e4722954eb6c6ae3ed20cf59493
migbro <miguel.a.brown@gmail.com>
2020-04-29 17:55:01 -0400

    :hammer: changed output to stdout do that it can be piped to bgzip

--- dev/create_pseudo_vcf.py
+++ dev/create_pseudo_vcf.py
@@ -7,16 +7,16 @@ import pdb
 parser = argparse.ArgumentParser(description='Create simulated vcf')
 parser.add_argument('-r', '--reference-fasta', action='store', dest='fasta', help='Reference fasta to check')
 parser.add_argument('-i', '--reference-index', action='store', dest='fai', help='Reference fai to populate vcf header')
-parser.add_argument('-o', '--output-basename', action='store', dest='out', help='Output vcf base name')
+# parser.add_argument('-o', '--output-basename', action='store', dest='out', help='Output vcf base name')
 
 args = parser.parse_args()
-out_vcf = open(args.out + ".vcf", "w")
+# out_vcf = open(args.out + ".vcf", "w")
 # output header
-out_vcf.write("##fileformat=VCFv4.2\n")
+sys.stdout.write("##fileformat=VCFv4.2\n")
 for line in open(args.fai):
     chr_info = line.rstrip('\n').split('\t')
-    out_vcf.write("##contig=<ID=" + chr_info[0] + ",length=" + chr_info[1] + ">\n")
-out_vcf.write("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tSIMULATED\n")
+    sys.stdout.write("##contig=<ID=" + chr_info[0] + ",length=" + chr_info[1] + ">\n")
+sys.stdout.write("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tSIMULATED\n")
 
 chrom = ""
 pos = 1
@@ -41,7 +41,7 @@ for line in open(args.fasta):
     else:
         for i in range(0, len(line.rstrip("\n")), 1):
             if line[i] in snp_dict:
-                out_vcf.write("\t".join([chrom, str(pos), ".", line[i], snp_dict[line[i]], ".\t.\t.", "GT", "0/1"]) + "\n")
+                sys.stdout.write("\t".join([chrom, str(pos), ".", line[i], snp_dict[line[i]], ".\t.\t.", "GT", "0/1"]) + "\n")
             pos += 1
     x += 1
-out_vcf.close()
+# out_vcf.close()
